90d7a60e8ffc2483e9b7f54d04542a9a29013add
mokim <mamokim2005@gmail.com>
2026-05-16 01:42:17 +0600

    updated linkdin links

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -64,7 +64,7 @@ const Contact = () => {
     { icon: <Github size={20} />, href: "https://github.com/Mokim2005" },
     {
       icon: <Linkedin size={20} />,
-      href: "https://www.linkedin.com/in/abdul-mokim-810380352",
+      href: "https://www.linkedin.com/in/abdul-mokim1/",
     },
     { icon: <Twitter size={20} />, href: "https://x.com/AbdulMokim40428" },
   ];
